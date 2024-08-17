c08cea383406b4deb9cd14fb4f11fbfce6ac44d1
Tommy Thorn <tommy-github2@thorn.ws>
2024-08-17 13:12:50 -0700

    Sequential multiplier

--- src/project.v
+++ src/project.v
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+`timescale 1ns / 1ps
 `default_nettype none
 `define N 23
 
@@ -20,6 +21,7 @@ module tt_um_tommythorn_experiments (
    reg [`N-1:0]	      a;
    reg [`N-1:0]	      aa;
 
+`ifdef SIMPLE_MULT   
    always @(posedge clk)
      if (rst_n == 0)
        a <= 0;
@@ -27,10 +29,63 @@ module tt_um_tommythorn_experiments (
 	aa <= a * a;
 	a <= a + 1;
      end
+`else
+   /* Iterative multiplier using carry-save */
+   reg s0;
+   reg [`N-1:0] sp, sn, c, a1;
+   wire [`N-1:0] add = c & 1 ? a1 : 0;
+   always @(posedge clk)
+     if (rst_n == 0) begin
+	a <= 0;
+	s0 <= 1;
+     end else if (s0) begin
+	sp <= 0;
+	sn <= ~0;
+	c <= a;
+	a1 <= a;
+	a <= a + 1;
+	s0 <= 0;
+	$display("");
+	$display("Start %1d * %1d", a, a);
+     end else /* !s0 */ begin
+	if (c == 0 && sp == 0) begin
+	   aa <= ~sn;
+	   s0 <= 1;
+	   $display("  iterate %d, %d, %d, %d, %d", sp, ~sn, c, a1, add);
+	   $display("Result %d", ~sn);
+	end else begin
+	   $display("  iterate %d, %d, %d, %d, %d", sp, ~sn, c, a1, add);
+           sp <= (sp & ~sn | add & (sp | ~sn)) << 1;
+	   sn <= sp ^ sn ^ add;
+           c <= c >> 1;
+           a1 <= a1 << 1;
+	end
+     end
+`endif
 	
    assign uio_oe  = ~0;
    assign {uo_out,uio_out} = aa[`N-1:`N-16];
 
    // List all unused inputs to prevent warnings
    wire _unused = &{ena, clk, rst_n, 1'b0, ui_in, uio_in };
+
 endmodule
+
+`ifdef SIM
+module tb;
+   reg clk, rst_n;
+   
+   tt_um_tommythorn_experiments insn(.clk(clk), .rst_n(rst_n));
+   always #5 clk = !clk;
+   initial begin
+      clk = 1;
+      rst_n = 0;
+
+      $display("Starting Sim");
+      
+      #20
+	rst_n = 1;
+      #10000 $finish;
+   end
+endmodule
+`endif
